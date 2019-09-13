25e50c77a039d25196f1abd8ebc98752ea21983a
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-09-13 19:52:46 -0400

    Part 1 finished?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-
+import CharacterList from './components/CharacterList.js';
+import EpisodeList from './components/EpisodeList.js';
+import LocationsList from './components/LocationsList.js';
 
 export default function App() {
   return (
     <main>
       <Header />
       <TabNav />
+      <CharacterList />
+      <EpisodeList />
+      <LocationsList />
     </main>
   );
 }
